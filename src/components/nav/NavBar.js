8e8b8d1b0e93320bbde0572589d7219af3db4485
michellecaner <michellecaner@gmail.com>
2022-03-07 21:18:54 -0600

    set up basic file structure and boilerplate login/register code

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export const NavBar = (props) => {
+  return (
+    
+    <nav className="navbar bg-dark text-white flex-md-nowrap p-0 shadow">
+      
+      <ul className="nav nav-pills nav-fill">
+        
+        <li className="nav-item">
+          <Link className="nav-link" to="/"><img src={'/images/image2vector.svg'} alt="home" /></Link>
+            </li>
+            
+        <li className="nav-item">
+          <Link className="nav-link" to="/projects">My Projects</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link" to="/categories">Catagories</Link>
+        </li>
+
+        <li className="nav-item">
+          <Link className="nav-link" to="/complete-projects">Complete Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
