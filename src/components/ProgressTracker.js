3b717d2399055bab642aba1ba436dc58a785da5b
michellecaner <michellecaner@gmail.com>
2022-03-12 09:18:18 -0600

    login and nav bar functioning

---
+++ src/components/ProgressTracker.js
@@ -0,0 +1,29 @@
+import React,  {useState} from "react"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import "./ProgressTracker.css"
+
+export const ProgressTracker = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(sessionStorage.getItem("nutshell_user") !== null)
+
+  const setAuthUser = (user) => {
+      sessionStorage.setItem("nutshell_user", JSON.stringify(user))
+      setIsAuthenticated(sessionStorage.getItem("nutshell_user") !== null)
+  }
+
+  const clearUser = () => {
+      sessionStorage.clear();
+      setIsAuthenticated(sessionStorage.getItem("nutshell_user") !== null)
+  }
+
+  return (
+    <>
+      <NavBar clearUser={clearUser} isAuthenticated={isAuthenticated}/>
+      <ApplicationViews 
+          setAuthUser={setAuthUser}
+          isAuthenticated={isAuthenticated}
+          setIsAuthenticated={setIsAuthenticated}
+      />
+    </>
+  )
+}
