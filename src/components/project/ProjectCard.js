2ddcb5bd2bbff37bd6d0bd160a084373b14368a8
michellecaner <michellecaner@gmail.com>
2022-03-14 20:56:10 -0500

    added delete button to projects

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,23 +1,24 @@
 import React from "react"
 import "./Project.css"
 
-export const ProjectCard = ({key, project}) => {
+export const ProjectCard = ({project, handleDeleteProject}) => {
     return (
-    <div className="card">
-        <div className="card-content">
-          <picture>
-            <img src={'/images/icons8-project-64.png'} alt="Project Icon" />
-          </picture>
-          <h3><span className="project__title">{project.title}
-          </span></h3>
-          <div className="project__description">{project.description}</div>
-          <div className="project__category">
+      <div className="card">
+          <div className="card-content">
             <picture>
-              <img src={'/images/icons8-category-48.png'} alt="Category Icon" />
+              <img src={'/images/icons8-project-64.png'} alt="Project Icon" />
             </picture>
-            {project.category.type}
+            <h3><span className="project__title">{project.title}
+            </span></h3>
+            <div className="project__description">{project.description}</div>
+            <div className="project__category">
+              <picture>
+                <img src={'/images/icons8-category-48.png'} alt="Category Icon" />
+              </picture>
+              <div className="project__category__type">{project.category.type}</div>
+            </div>
+              <button type="button" onClick={() => handleDeleteProject(project.id)}>Delete</button>
           </div>
-        </div>
-    </div>
+      </div>  
     )
-  }
+}
