fd5c30e9d224d065dfbadf58ad79cc3f1373b730
michellecaner <michellecaner@gmail.com>
2022-03-15 19:13:18 -0500

    added delete button to project details

--- src/components/project/ProjectDetail.js
+++ src/components/project/ProjectDetail.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from "react-router-dom"
-import { getProjectById } from '../../modules/ProjectManager';
+import { deleteProject, getProjectById } from '../../modules/ProjectManager';
 import './ProjectDetail.css';
 
 
 export const ProjectDetail = () => {
   const [project, setProject] = useState({ description: "", category: "" });
+  const [isLoading, setIsLoading] = useState(true);
 
   const {projectId} = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
-    //getPropjectById(id) from ProjectManager and hang on to the data; put it into state
+    //getProjectById(id) from ProjectManager and hang on to the data; put it into state
     console.log("useEffect", projectId)
     getProjectById(projectId)
       .then(project => {
@@ -19,12 +20,25 @@ export const ProjectDetail = () => {
       });
   }, [projectId]);
 
+  const handleDelete = () => {
+    //invoke the delete function in AnimalManger and re-direct to the animal list.
+    setIsLoading(true);
+    deleteProject(projectId).then(() =>
+      navigate("/projects")
+    );
+  };
+
   return (
     <section className="project">
       <h3 className="project__title">{project.title}</h3>
       <div className="project__description">{project.description}</div>
       <div className="project__user">User: {project.user?.name}</div>
       <div className="project__category">Category: {project.category?.type}</div>
+
+      <button type="button" disabled={isLoading} onClick={handleDelete}>
+          Delete
+      </button>
+      
     </section>
   );
 }
